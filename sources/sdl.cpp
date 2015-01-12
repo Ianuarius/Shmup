bcafbf25d38ea7e33f62939d653b6994f8001348
Juha Keränen <juha.keranen85@gmail.com>
2015-01-12 17:36:21 +0200

    sdl sourcet

---
+++ sources/sdl.cpp
@@ -0,0 +1,27 @@
+#include "sdl.h"
+
+bool SDL::init()
+{
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0 
+	{
+		printf ("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+
+	if (TTF_Init() == -1)
+	{
+		printf("SDL TTF could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void SDL::exit()
+{
+	TTF_Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
---
+++ sources/sdl.h
@@ -0,0 +1,20 @@
+#ifndef __SDL_H_INCLUDED__
+#define __SDL_H_INCLUDED__
+
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+// High level wrapper for SDL2 calls
+
+namespace SDL
+{
+	// Initializes SDL2, must be called before everything else
+	bool init();
+
+	// Destroys SDL2, must be called when program closes
+	void exit();
+}
+
+#endif // __SDL_H_INCLUDED__
