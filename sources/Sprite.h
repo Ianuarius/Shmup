bb240b16dc48273dccda4a63a96cf05cc80e718c
Juha Keränen <juha.keranen85@gmail.com>
2015-02-12 13:24:35 +0200

    Update Sprite.h
    Lisätty kommentointia.

--- sources/Sprite.h
+++ sources/Sprite.h
@@ -10,9 +10,11 @@
 class Sprite
 {
 	public:
+		// Osoitin pelin ikkunaan, spriten kuvatiedoston nimi ja yhden tiedostossa olevan spriten koko.
 		Sprite(Window *window, std::string filename, int width, int height);
 		virtual ~Sprite();
 
+		// Renderöi spriteSheet-vektorista osan, joka on määrätty 'index'-argumenttiin annetun arvon perusteella.
 		void render(int index, int x, int y);
 
 	private:
@@ -22,6 +24,7 @@ class Sprite
 		int width;
 		int height;
 
+		// Vektori, johon on tallennettu spritet määrätyn kokoisina palasina.
 		std::vector<SDL_Rect> spriteSheet;
 };
 
